bd6434c1e9b3908549808602c5b30b51f4019657
Basile Atsougan <atsougan870@gmail.com>
2024-10-06 10:38:31 +0200

    Handling API request

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,40 +4,49 @@ import axios from 'axios';
 const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const [error, seError] = useState(null);
+    const [error, setError] = useState(null);
 
 
-    const axios = require('axios');
 
     const options = {
     method: 'GET',
-    url: `https://jsearch.p.rapidapi.com/estimated-salary/${endpoint}`,
-    params: { ...query
-        // job_title: 'NodeJS Developer',
-        // location: 'New-York, NY, USA',
-        // radius: '100'
-    },
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
     headers: {
         'x-rapidapi-key': '9b493e83e2mshbe6cd7f97bff50dp19d07ajsn3a915e020448',
         'x-rapidapi-host': 'jsearch.p.rapidapi.com'
     }
     };
 
+
     const fetchData = async() => {
         setIsLoading(true);
-    
+        try {
+            const response = await axios.request(options);
 
-    try {
-        const response = await axios.request
-        (options)
+            setData(response.data.data);
+            setIsLoading(false);
+        } catch (error) {
 
-    } catch (error) {
-
-    } finally {
-
+            alert('There is an error')
+            setError(error)
+        } finally {
+            setIsLoading(false);
+        }
     }
 
-}
+    useEffect (() => {
+        fetchData();
+    }, []);
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    };
 
 
-}
+    return { data, isLoading, error, refetch };
+};   
+
+
+export default useFetch;
